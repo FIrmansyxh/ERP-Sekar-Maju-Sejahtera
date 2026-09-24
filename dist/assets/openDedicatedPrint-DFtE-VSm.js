ad56e493af07e80cf52878c4e37003f4dc1c5774
FIrmansyxh <muhammadfirmansyah220@gmail.com>
2026-09-24 14:04:52 +0700

    FE-BE V1

---
+++ dist/assets/openDedicatedPrint-DFtE-VSm.js
@@ -0,0 +1,1 @@
+function e(n,o){try{window.dispatchEvent(new CustomEvent("erp-open-print-doc",{detail:{type:n,id:o}}))}catch(r){console.error("Error dispatching erp-open-print-doc:",r)}}export{e as o};
